01682408ab57d2e853a2f9111166276957436b6e
mohit355 <mohit_bt18@iiitkalyani.ac.in>
2021-01-16 02:30:01 +0530

    added hamburger for togle button

--- src/components/Navigation/SideDrawer/DrawerToggler/DrowerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggler/DrowerToggle.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./DrowerToggle.css";
 const drowerToggle = (props) => {
-  return <div onClick={props.clicked}>MENU</div>;
+  return (
+    <div className="DrawerToggle" onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default drowerToggle;
